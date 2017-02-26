4a401de331e333f3e0c2b77a933c34da3cc461a5
TusharBalar <balar.tushar1@gmail.com>
2017-02-26 22:40:00 +0530

    Dashboard updated

--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ModalController } from 'ionic-angular';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
 import { initializeApp, database } from 'firebase';
 import { AppService } from '../../service/app.service';
+
+import { Account } from './account/account';
+import { NewRequest } from './newRequest/newRequest';
+import { AllRequest } from './allRequest/allRequest';
 
 @Component({
   selector: 'dashboard',
@@ -18,20 +22,24 @@ export class Dashboard {
 
   constructor(public navCtrl: NavController,
               public appService: AppService,
+              public modalCtrl: ModalController,
               private af: AngularFire) {
     
   }
 
   allRequest() {
-    
+    let allRequest = this.modalCtrl.create(AllRequest);
+    allRequest.present();
   }
 
   newRequest() {
-
+    let newRequest = this.modalCtrl.create(NewRequest);
+    newRequest.present();
   }
 
   account() {
-
+    let account = this.modalCtrl.create(Account);
+    account.present();
   }
 
 
