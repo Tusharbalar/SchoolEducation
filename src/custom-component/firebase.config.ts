2a03b2ad3f5a5768272b17629397dceea9117f11
TusharBalar <balar.tushar1@gmail.com>
2017-02-26 15:42:12 +0530

    Update firebase.config.ts

--- src/custom-component/firebase.config.ts
+++ src/custom-component/firebase.config.ts
@@ -1,5 +1,5 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyDKg7_pIlXmas7lhXo9evjagsZDi5qM3f8",
+  apiKey: "",
   authDomain: "schooleducation-6ef39.firebaseapp.com",
   databaseURL: "https://schooleducation-6ef39.firebaseio.com",
   storageBucket: "schooleducation-6ef39.appspot.com",
