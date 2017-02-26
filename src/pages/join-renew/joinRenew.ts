ca6753e987bd314fab389e437bb6d1f86ad0452a
TusharBalar <balar.tushar1@gmail.com>
2017-02-26 17:19:49 +0530

    Join/Renew page update

--- src/pages/join-renew/joinRenew.ts
+++ src/pages/join-renew/joinRenew.ts
@@ -11,6 +11,7 @@ export class JoinRenew {
 
   public page1: boolean = true;
   public page2: boolean = false;
+  public page3: boolean = false;
 
   public title: string = "Join/Renew";
 
@@ -21,6 +22,17 @@ export class JoinRenew {
   onSubmit() {
     this.page1 = false;
     this.page2 = true;
+    this.page3 = false;
+  }
+
+  makePayment() {
+    this.page1 = false;
+    this.page2 = false;
+    this.page3 = true;
+  }
+
+  donePayment() {
+    console.log("DSADAS")
   }
 
 }
