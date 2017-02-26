4d3aef11ee03354e8849f396516c927350fc418e
TusharBalar <balar.tushar1@gmail.com>
2017-02-26 15:42:39 +0530

    Join/Renew page start

--- src/pages/join-renew/joinRenew.ts
+++ src/pages/join-renew/joinRenew.ts
@@ -9,10 +9,18 @@ import { NavController } from 'ionic-angular';
 
 export class JoinRenew {
 
+  public page1: boolean = true;
+  public page2: boolean = false;
+
   public title: string = "Join/Renew";
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  onSubmit() {
+    this.page1 = false;
+    this.page2 = true;
   }
 
 }
